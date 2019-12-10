a473f7def99e2c072917258c11cc3de3439c8f33
kensiah <siahkianann0479@e.newera.edu.my>
2019-12-10 18:47:25 +0800

    AP finals

--- app/src/main/java/com/example/apfinals/LoginActivity.java
+++ app/src/main/java/com/example/apfinals/LoginActivity.java
@@ -2,11 +2,9 @@ package com.example.apfinals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,7 +18,6 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
